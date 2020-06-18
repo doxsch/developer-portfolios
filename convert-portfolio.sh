53f86c663ee08d18f1667cc2e8d02debc459b25a
doxsch <28098153+doxsch@users.noreply.github.com>
2020-06-18 21:26:21 +0200

    keep data folder removed debug code

--- convert-portfolio.sh
+++ convert-portfolio.sh
@@ -3,8 +3,6 @@
 IFS_backup=$IFS
 IFS=$'\n'
 
-pwd
-ls -alh
 # find name from portfolios.md
 names=($(grep '*' portfolios.md | awk -F"[" '{print $2}' | awk -F"]" '{print $1}'))
 
@@ -21,7 +19,6 @@ length=${#urls[@]}
 echo "Found $length urls..."
 
 [ -e ./src/data/sites.yaml ] && rm ./src/data/sites.yaml
-touch ./src/data/sites.yaml
 echo "# This file is created by a script, so do not edit it manually"> ./src/data/sites.yaml
 for i in "${!names[@]}"
 do
