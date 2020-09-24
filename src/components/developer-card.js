5b6c29ff0e15da83b3456cbe54344825ca84e0dd
doxsch <28098153+doxsch@users.noreply.github.com>
2020-09-24 19:29:59 +0200

    removed fallback solution for not reachable pages

--- src/components/developer-card.js
+++ src/components/developer-card.js
@@ -23,7 +23,7 @@ const DeveloperCard = ({name, url, image}) => {
                             fadeIn={true}
 
                         /> :
-                        <DefaultImage/>
+                        <></>
                     }
 
                 </a>
